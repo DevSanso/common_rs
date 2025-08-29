4cdfaf43cd023cab83d69fb861625c351542dccf
Sunken Ahn <devsanso@outlook.com>
2025-08-29 23:15:50 +0900

    err 매개변수 옵션화

--- common_rs/src/init.rs
+++ common_rs/src/init.rs
@@ -4,6 +4,9 @@ pub mod err_code;
 
 use std::error::Error;
 
+use common_conn::err::COMMON_CONN_ERROR_CATEGORY;
+use common_core::err::API_CALL_ERROR;
+
 use crate::ErrorCategory;
 use crate::ErrorCode;
 use crate::ErrorDesc;
@@ -18,13 +21,18 @@ pub struct ErrorCodeConfig {
     pub category_id :ErrorCategory, 
     pub errs : Vec<(ErrorCode, ErrorDesc)>
 }
-pub fn init_common(logger : LoggerConfig<'_>, errors : ErrorCodeConfig) -> Result<(), Box<dyn Error>>{
+pub fn init_common(logger : LoggerConfig<'_>, errors : Option<ErrorCodeConfig>) -> Result<(), Box<dyn Error>>{
     signal::init_once();
 
     logger::init_once(logger.log_level, logger.log_file)?;
 
-    if errors.category_id == COMMON_ERROR_CATEGORY {
-        //return crate::error_code::common_make_err!(COMMON_ERROR_CATEGORY, ApiCallError)
+    if errors.is_some() {
+        let e = errors.unwrap();
+        if e.category_id == COMMON_ERROR_CATEGORY || e.category_id == COMMON_CONN_ERROR_CATEGORY {
+            return crate::err::create_error(COMMON_ERROR_CATEGORY,
+                 API_CALL_ERROR, format!("already exists {}", e.category_id), None).as_error();
+            //return crate::error_code::common_make_err!(COMMON_ERROR_CATEGORY, ApiCallError)
+        }
     }
 
     common_conn::err::common_conn_init();
