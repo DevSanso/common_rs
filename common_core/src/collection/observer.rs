d917b599bc884c4a03c7e8506a9ada1119118950
Sunken Ahn <devsanso@outlook.com>
2025-12-09 21:44:34 +0900

    threadsafeObserver

--- common_core/src/collection/observer.rs
+++ common_core/src/collection/observer.rs
@@ -1,5 +1,5 @@
 use std::sync::atomic::{AtomicU64, Ordering};
-
+use std::sync::Arc;
 pub struct ChangeSubject<T : 'static + Clone> {
     current : Option<T>,
     seq     : AtomicU64
@@ -8,6 +8,10 @@ pub struct ChangeSubject<T : 'static + Clone> {
 impl<T : 'static + Clone> ChangeSubject<T> {
     pub fn new() -> ChangeSubject<T> {
         ChangeSubject { current : None, seq: AtomicU64::new(0) }
+    }
+
+    pub fn new_arc() -> Arc<ChangeSubject<T>> {
+        Arc::new(ChangeSubject { current : None, seq: AtomicU64::new(0) })
     }
 
     pub fn notify(&mut self, val : T) {
@@ -32,6 +36,31 @@ impl<'a, T : 'static + Clone> ChangeObserver<'a, T> {
     pub fn update(&self) -> Option<T> {
         if self.observer.seq.load(Ordering::SeqCst) == self.seq.load(Ordering::Relaxed) {
            None
+        } else {
+            let ret = self.observer.current.as_ref().cloned();
+            self.seq.store(self.observer.seq.load(Ordering::SeqCst), Ordering::Relaxed);
+            ret
+        }
+    }
+}
+
+pub struct ChangeThreadSafeObserver<T : 'static + Clone> {
+    observer : Arc<ChangeSubject<T>>,
+    seq     : AtomicU64
+}
+
+impl<T : 'static + Clone> ChangeThreadSafeObserver<T> {
+    pub fn subscribe(subject : Arc<ChangeSubject<T>>) -> ChangeThreadSafeObserver<T> {
+        let seq = subject.seq.load(Ordering::SeqCst);
+        ChangeThreadSafeObserver {
+            observer : subject,
+            seq      : AtomicU64::new(seq),
+        }
+    }
+
+    pub fn update(&self) -> Option<T> {
+        if self.observer.seq.load(Ordering::SeqCst) == self.seq.load(Ordering::Relaxed) {
+            None
         } else {
             let ret = self.observer.current.as_ref().cloned();
             self.seq.store(self.observer.seq.load(Ordering::SeqCst), Ordering::Relaxed);
