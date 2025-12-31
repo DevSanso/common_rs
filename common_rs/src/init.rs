8d7b55b4af3d4f829c5a45ee7b110d9837a4e751
Sunken Ahn <devsanso@outlook.com>
2025-12-31 13:08:09 +0900

    log 라이브러리 변경, 로그별 파일 별개로 설정 되도록 변경

--- common_rs/src/init.rs
+++ common_rs/src/init.rs
@@ -7,11 +7,11 @@ mod logger;
 pub struct InitConfig<'a> {
     pub log_level : &'a str,
     pub log_file : Option<&'a str>,
-    pub log_file_size_mb : u64,
+    pub log_file_size : usize,
 }
 pub fn init_common(cfg : InitConfig<'_>) -> Result<(), CommonError>{
     signal::init_once();
-    logger::init_once(cfg.log_level, cfg.log_file, cfg.log_file_size_mb).map_err(|e| {
+    logger::init_once(cfg.log_level, cfg.log_file, cfg.log_file_size).map_err(|e| {
         CommonError::extend(&CommonDefaultErrorKind::InitFailed, "can't success init_once", e)
     })?;
     Ok(())
--- common_rs/src/init/logger.rs
+++ common_rs/src/init/logger.rs
@@ -1,66 +1,101 @@
 use std::error::Error;
+use std::fs::File;
+use std::num::NonZeroUsize;
 use std::path::Path;
-use std::sync::Once;
+use std::sync::{Once, OnceLock};
 
-use ftail::Ftail;
-use log::LevelFilter;
+use logforth::append;
+use logforth::record::Level;
+use logforth::record::LevelFilter;
 use common_err::CommonError;
 use common_err::gen::CommonDefaultErrorKind;
+use logforth;
+use logforth::append::file::FileBuilder;
+use logforth::starter_log::LogStarterBuilder;
 
-fn convert_str_to_log_level(log_level : &'_ str) -> LevelFilter {
+fn convert_str_to_log_level(log_level : &'_ str) -> Level {
     match log_level {
-        "debug" => LevelFilter::Debug,
-        "warn" => LevelFilter::Warn,
-        "trace" => LevelFilter::Trace,
-        "info" => LevelFilter::Info,
-        _ => LevelFilter::Error
+        "debug" => Level::Debug,
+        "warn" => Level::Warn,
+        "trace" => Level::Trace,
+        "info" => Level::Info,
+        _ => Level::Error
     }
 }
 
 static LOGGER_INIT_ONCE : Once = Once::new();
-pub(crate) static mut LOGGER_FILE_LEVEL_IS_TRACE : bool = false;
 
-pub fn init_once(log_level : &'_ str, log_file : Option<&'_ str>, max_size : u64) -> Result<(), CommonError> {
+fn set_log_builder(level: Level, base_dir_opt : Option<&'_ str>, max_size : usize, builder : LogStarterBuilder) -> Result<LogStarterBuilder, CommonError> {
+    const LOG_LEVEL_FILE_NAME : &[&'static str] = &[
+        "error.log",
+        "warn.log",
+        "info.log",
+        "debug.log",
+        "trace.log",
+    ];
+
+    const LOG_LEVEL_THRESHOLD : &[Level] = &[
+        Level::Error,
+        Level::Warn,
+        Level::Info,
+        Level::Debug,
+        Level::Trace
+    ];
+
+    let mut dispatch = builder;
+    let max_idx = LOG_LEVEL_THRESHOLD.iter().position(|l| *l == level).map_or(0, |i| i);
+
+    for dispatch_idx in 0..max_idx {
+        if let Some(base_dir) = base_dir_opt.as_ref() {
+            let file = FileBuilder::new(base_dir, LOG_LEVEL_FILE_NAME[dispatch_idx])
+                .rollover_size(NonZeroUsize::new(max_size).expect("NonZeroUsize New is Broken"))
+                .max_log_files(NonZeroUsize::new(5).expect("NonZeroUsize New is Broken"))
+                .build()
+                .map_err(|e| {
+                    CommonError::new(&CommonDefaultErrorKind::ThirdLibCallFail, e.to_string())
+                })?;
+
+            dispatch = dispatch.dispatch(|d| {
+                d.filter(LevelFilter::MoreSevereEqual(LOG_LEVEL_THRESHOLD[dispatch_idx]))
+                    .append(file)
+            });
+        } else {
+            let stdout = append::Stdout::default();
+            dispatch = dispatch.dispatch(|d| {
+                d.filter(LevelFilter::MoreSevereEqual(LOG_LEVEL_THRESHOLD[dispatch_idx]))
+                    .append(stdout)
+            });
+        }
+    }
+    Ok(dispatch)
+}
+pub(crate) static LOGGER_FILE_LEVEL_IS_TRACE : OnceLock<bool> = OnceLock::new();
+
+pub fn init_once(log_level : &'_ str, log_dir : Option<&'_ str>, max_size : usize) -> Result<(), CommonError> {
     let mut ret : Result<(), CommonError> = Ok(());
+
+    if let Some(log_dir_path) = log_dir {
+        if !std::fs::exists(log_dir_path).map_err(|e| {
+            CommonError::new(&CommonDefaultErrorKind::SystemCallFail, e.to_string())
+        })? {
+            return CommonError::new(&CommonDefaultErrorKind::SystemCallFail, format!("not exists dir {}", log_dir_path)).to_result();
+        }
+    }
 
     LOGGER_INIT_ONCE.call_once(|| {
         let level = convert_str_to_log_level(log_level);
-        let mut ftail = Ftail::new().datetime_format("%Y-%m-%d %H:%M:%S%.3f").max_file_size(max_size);
+        if level == Level::Trace {
+            LOGGER_FILE_LEVEL_IS_TRACE.get_or_init(|| true);
+        }
 
-        if level == LevelFilter::Trace {
-            ftail = ftail.console(LevelFilter::Trace);
+        let builder = set_log_builder(level, log_dir, max_size,logforth::starter_log::builder()).map_err(|e| {
+            CommonError::extend(&CommonDefaultErrorKind::InitFailed, "log builder failed", e).to_result()
+        });
+
+        if let Err(e) = builder {
+            ret = e
         } else {
-            ftail = ftail.console(LevelFilter::Debug);
-        }
-
-        if log_file.is_some() {
-            let file_path = log_file.unwrap();
-            {
-                let chk_write = std::fs::OpenOptions::new()
-                    .write(true)
-                    .create(true)
-                    .append(true)
-                    .open(file_path.trim().to_string());
-
-                if chk_write.is_err() {
-                    ret = CommonError::new(&CommonDefaultErrorKind::SystemCallFail, format!("common_rs - logger,init,chk - {}:{}", file_path, chk_write.err().unwrap()))
-                        .to_result();
-                    return;
-                }
-            }
-            ftail = ftail.single_file(Path::new(file_path), true, level);
-        }
-
-        unsafe {
-            if level == LevelFilter::Trace {
-                LOGGER_FILE_LEVEL_IS_TRACE = true;
-            }
-
-            ret = match ftail.init() {
-                Ok(_) => Ok(()),
-                Err(e) => CommonError::new(&CommonDefaultErrorKind::InvalidApiCall, format!("common_rs - logger,init,console\
-                 - {}", e)).to_result()
-            }
+            builder.expect("builder expect is broken").apply()
         }
     });
 
