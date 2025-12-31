ff3fc15442b406349c2c061a3e9c8eaaa8c84a26
Sunken Ahn <devsanso@outlook.com>
2026-01-01 00:41:29 +0900

    log 자체에서 func, file, line 지원으로 인하여, 불필요 로직 제거

--- common_core/src/lib.rs
+++ common_core/src/lib.rs
@@ -8,13 +8,8 @@ pub mod logger {
     macro_rules! log_debug {
         ($($arg:tt)+) => {
             {
-                let caller = std::panic::Location::caller();
-                let func = $crate::utils::macros::func!();
-                let line = caller.line();
-                let file = caller.file();
                 let thread_id = std::thread::current().id();
-
-                $crate::logger::log::debug!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, func, format!($($arg)*));
+                $crate::logger::log::debug!("{:?} at {:.1024}", thread_id, format!($($arg)*));
             }
         };
     }
@@ -23,13 +18,8 @@ pub mod logger {
     macro_rules! log_warn {
         ($($arg:tt)+) => {
             {
-                let caller = std::panic::Location::caller();
-                let func = $crate::utils::macros::func!();
-                let line = caller.line();
-                let file = caller.file();
                 let thread_id = std::thread::current().id();
-
-                $crate::logger::log::warn!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, func, format!($($arg)*));
+                $crate::logger::log::warn!("{:?} at {:.1024}", thread_id, format!($($arg)*));
             }
         };
     }
@@ -38,13 +28,8 @@ pub mod logger {
     macro_rules! log_trace {
         ($($arg:tt)+) => {
             {
-                let caller = std::panic::Location::caller();
-                let func = $crate::utils::macros::func!();
-                let line = caller.line();
-                let file = caller.file();
                 let thread_id = std::thread::current().id();
-
-                $crate::logger::log::trace!("{:?} at {}:{}:{}\n{}", thread_id, file, line, func, format!($($arg)*));
+                $crate::logger::log::trace!("{:?} at {}", thread_id, format!($($arg)*));
             }
         };
     }
@@ -53,13 +38,8 @@ pub mod logger {
     macro_rules! log_error {
         ($($arg:tt)+) => {
             {
-                let caller = std::panic::Location::caller();
-                let func = $crate::utils::macros::func!();
-                let line = caller.line();
-                let file = caller.file();
                 let thread_id = std::thread::current().id();
-
-                $crate::logger::log::error!("{:?} at {}:{}:{}\n{}", thread_id, file, line, func, format!($($arg)*));
+                $crate::logger::log::error!("{:?} at \n{}", thread_id, format!($($arg)*));
             }
         };
     }
@@ -68,11 +48,8 @@ pub mod logger {
     macro_rules! log_info {
         ($($arg:tt)+) => {
             {
-                let caller = std::panic::Location::caller();
-                let func = $crate::utils::macros::func!();
                 let thread_id = std::thread::current().id();
-
-                $crate::logger::log::info!("{:?} at {}\n{:.1024}", thread_id, func, format!($($arg)*));
+                $crate::logger::log::info!("{:?} at {:.1024}", thread_id, format!($($arg)*));
             }
         };
     }
