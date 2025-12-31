9bceaad12c3ad32cbc3c696d510381f4ae275952
Sunken Ahn <devsanso@outlook.com>
2025-12-31 23:18:14 +0900

    인자값 가변으로 변경

--- common_core/src/lib.rs
+++ common_core/src/lib.rs
@@ -6,79 +6,79 @@ pub mod logger {
     pub use log;
     #[macro_export]
     macro_rules! log_debug {
-        ($message:expr) => {
+        ($($arg:tt)+) => {
             {
                 let caller = std::panic::Location::caller();
                 let func = $crate::utils::macros::func!();
                 let line = caller.line();
                 let file = caller.file();
                 let thread_id = std::thread::current().id();
-                
-                $crate::logger::log::debug!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, line, $message);
+
+                $crate::logger::log::debug!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, func, format!($($arg)*));
             }
         };
     }
 
     #[macro_export]
     macro_rules! log_warn {
-        ($message:expr) => {
+        ($($arg:tt)+) => {
             {
                 let caller = std::panic::Location::caller();
                 let func = $crate::utils::macros::func!();
                 let line = caller.line();
                 let file = caller.file();
                 let thread_id = std::thread::current().id();
-                
-                $crate::logger::log::warn!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, line, $message);
+
+                $crate::logger::log::warn!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, func, format!($($arg)*));
             }
         };
     }
 
     #[macro_export]
     macro_rules! log_trace {
-        ($message:expr) => {
+        ($($arg:tt)+) => {
             {
                 let caller = std::panic::Location::caller();
                 let func = $crate::utils::macros::func!();
                 let line = caller.line();
                 let file = caller.file();
                 let thread_id = std::thread::current().id();
-                
-                $crate::logger::log::trace!("{:?} at {}:{}:{}\n{}", thread_id, file, line, line, $message);
+
+                $crate::logger::log::trace!("{:?} at {}:{}:{}\n{}", thread_id, file, line, func, format!($($arg)*));
             }
         };
     }
 
     #[macro_export]
     macro_rules! log_error {
-        ($message:expr) => {
+        ($($arg:tt)+) => {
             {
                 let caller = std::panic::Location::caller();
                 let func = $crate::utils::macros::func!();
                 let line = caller.line();
                 let file = caller.file();
                 let thread_id = std::thread::current().id();
-                
-                $crate::logger::log::error!("{:?} at {}:{}:{}\n{}", thread_id, file, line, line, $message);
+
+                $crate::logger::log::error!("{:?} at {}:{}:{}\n{}", thread_id, file, line, func, format!($($arg)*));
             }
         };
     }
 
     #[macro_export]
     macro_rules! log_info {
-        ($message:expr) => {
+        ($($arg:tt)+) => {
             {
                 let caller = std::panic::Location::caller();
                 let func = $crate::utils::macros::func!();
                 let line = caller.line();
                 let file = caller.file();
                 let thread_id = std::thread::current().id();
-                
-                $crate::logger::log::info!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, line, $message);
+
+                $crate::logger::log::info!("{:?} at {}:{}:{}\n{:.1024}", thread_id, file, line, func, format!($($arg)*));
             }
         };
     }
-    
+
     pub use log_debug;
     pub use log_warn;
     pub use log_info;
